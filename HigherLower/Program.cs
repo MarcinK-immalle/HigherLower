f6426243b96f9d034da60e4ddcc0895868b48b77
Dries Augustyns <dries.augustyns@immalle.be>
2017-11-28 09:48:56 +0100

    Made Higher-Lower game

--- HigherLower/Program.cs
+++ HigherLower/Program.cs
@@ -9,6 +9,28 @@ namespace HigherLower
     {
         static void Main(string[] args)
         {
+            Random RandomNumber = new Random();
+            int getal = RandomNumber.Next(1, 50);
+            string invoer ="";
+            int invoerGetal = 0;
+
+            while (invoerGetal != getal)
+            {
+                invoer = Console.ReadLine();
+                invoerGetal = Convert.ToInt32(invoer);
+                if (invoerGetal < getal)
+                {
+                    Console.WriteLine("Hoger!");
+                } 
+                else if (invoerGetal > getal)
+                {
+                    Console.WriteLine("Lager!");
+                }
+                else if (invoerGetal == getal)
+                {
+                    Console.WriteLine("Goed geraden! {0} was het getal!", getal);
+                }
+            }
         }
     }
 }
